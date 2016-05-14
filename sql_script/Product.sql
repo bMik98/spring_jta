539996acde95730b9752b505dda28716c4847b93
Maksim Pavlenko <maksimp86@mail.ru>
2016-05-14 22:39:20 +0300

    sql script

---
+++ sql_script/Product.sql
@@ -0,0 +1,13 @@
+create table product
+(
+id int primary key not null primary key,
+name varchar(40) not null,
+lastname varchar(60) not null,
+age int,
+phone int not null,
+email varchar(40),
+city varchar(30),
+street varchar(30),
+number_house int,
+number_apartment int
+);
